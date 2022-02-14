76952fe3c8af9ee10addef028a4cf42fe0e7fd89
CarlosGalleguillos <consultoria.pyme@gmail.com>
2022-02-14 15:26:06 -0300

    Ejercicio 3 terminado

--- EjerciciosPython/Ejercicios/Ejercicios-tdd.py
+++ EjerciciosPython/Ejercicios/Ejercicios-tdd.py
@@ -34,9 +34,9 @@ Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!."""
 Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable
  y luego muestre por pantalla el contenido de la variable."""
 
-a = "¡Hola Mundo!"
+#a = "¡Hola Mundo!"
 
-print(a)
+#print(a)
 
 
 
@@ -70,7 +70,8 @@ print(a)
 Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca 
 muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
 """
-
+nombre = input("¿Cuál es tu nombre?")
+print("¡Hola "+ nombre + "!")
 
 
 
